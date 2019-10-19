3c028115c4cc029c180900c7b4bbedf2887a4327
Thy H. Nguyen <46432072+nguyent2@users.noreply.github.com>
2019-10-19 15:12:21 -0400

    Create Asking_for_a_list.py

---
+++ Asking_for_a_list.py
@@ -0,0 +1,6 @@
+list = []
+number_of_elements = input("How many elements do you want to put in a list?")
+for i in range(int(number_of_elements)):
+  i = input("Enter an element")
+  list.append(i)
+print(list)
